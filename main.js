58a4750e147bcd4068b1e3519053b002e904c4f2
K-Ramberg <kramberg0350@gmail.com>
2018-05-21 18:04:21 -0400

    functioning array push methods. quad indicator working for user selection and comp initial selection

--- main.js
+++ main.js
@@ -1,33 +1,94 @@
-function selectIndicator (selectorNumber){
-    if (selectorNumber === 1){
-        $('#simon-selector-1').addClass("quadrant-selected pulse")}
-    if (selectorNumber === 2){
-        $('#simon-selector-2').addClass("quadrant-selected pulse")}
-    if (selectorNumber === 3){
-        $('#simon-selector-3').addClass("quadrant-selected pulse")}
-     if (selectorNumber === 4){
-        $('#simon-selector-4').addClass("quadrant-selected pulse")}
-}
-function unIndicate (selectorNumber){
-    if (selectorNumber === 1){
-        $('#simon-selector-1').removeClass("quadrant-selected pulse")}
-    if (selectorNumber === 2){
-        $('#simon-selector-2').removeClass("quadrant-selected pulse")}
-    if (selectorNumber === 3){
-        $('#simon-selector-3').removeClass("quadrant-selected pulse")}
-     if (selectorNumber === 4){
-        $('#simon-selector-4').removeClass("quadrant-selected pulse")}
+const gameFoundationState = {
+    difficulty: 'easy',
+    computerSequenceArray: [],
+    playerSelectionSequence: [],
+    gameLevel: 1,
+    currentTimeSet: function () {
+        if (this.difficulty === 'easy') {
+            return 1000
+        }
+        if (this.difficulty === 'medium') {
+            return 500
+        }
+        if (this.difficulty === 'hard') {
+            return 250
+        }
+    }
 }
 
-$('#simon-selector-1').click(function(){
-    selectIndicator(1);
+
+const GameRun = {
+    sequenceNumGenerate: function(){
+        const quadNumber = Math.ceil(Math.random() * 4)
+        return quadNumber
+    },
+    computerSimonSequence: function () {
+        const selection = this.sequenceNumGenerate();
+        FlasherGroup.onOffQuadrant(selection);
+        gameFoundationState.computerSequenceArray.push(selection)
+    },
+}
+
+
+const FlasherGroup = {
+    selectIndicator: function (selectorNumber) {
+        if (selectorNumber === 1) {
+            $('#simon-selector-1').addClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 2) {
+            $('#simon-selector-2').addClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 3) {
+            $('#simon-selector-3').addClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 4) {
+            $('#simon-selector-4').addClass("quadrant-selected pulse")
+        }
+    },
+    unIndicate: function (selectorNumber) {
+        if (selectorNumber === 1) {
+            $('#simon-selector-1').removeClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 2) {
+            $('#simon-selector-2').removeClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 3) {
+            $('#simon-selector-3').removeClass("quadrant-selected pulse")
+        }
+        if (selectorNumber === 4) {
+            $('#simon-selector-4').removeClass("quadrant-selected pulse")
+        }
+    },
+
+    onOffQuadrant: function (selectorNumber) {
+        this.selectIndicator(selectorNumber);
+        setTimeout(function () {
+            FlasherGroup.unIndicate(selectorNumber)
+        }, gameFoundationState.currentTimeSet())
+    }
+
+}
+
+
+
+$('#simon-selector-1').click(function () {
+    FlasherGroup.onOffQuadrant(1);
+    gameFoundationState.playerSelectionSequence.push(1)
 })
-$('#simon-selector-2').click(function(){
-    selectIndicator(2);
+$('#simon-selector-2').click(function () {
+    FlasherGroup.onOffQuadrant(2);
+    gameFoundationState.playerSelectionSequence.push(2)
 })
-$('#simon-selector-3').click(function(){
-    selectIndicator(3);
+$('#simon-selector-3').click(function () {
+    FlasherGroup.onOffQuadrant(3);
+    gameFoundationState.playerSelectionSequence.push(3)
 })
-$('#simon-selector-4').click(function(){
-    selectIndicator(4);
+$('#simon-selector-4').click(function () {
+    FlasherGroup.onOffQuadrant(4);
+    gameFoundationState.playerSelectionSequence.push(4)
+})
+
+
+$('#start-button').click(function () {
+    GameRun.computerSimonSequence();
 })
